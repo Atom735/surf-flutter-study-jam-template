6c33e76c5c20607037d5531d655644ccb27159cf
Atom735 <Atom735.ag47@gmail.com>
2022-02-20 15:11:57 +0300

    add avatar and colors

--- lib/src/chat/widgets/w_chat_msg_tile.dart
+++ lib/src/chat/widgets/w_chat_msg_tile.dart
@@ -8,9 +8,28 @@ class WChatMsgTile extends StatelessWidget {
   final ChatMessageData data;
 
   @override
-  Widget build(BuildContext context) => ListTile(
-        trailing: Text(data.timestamp.toString()),
-        title: Text(data.author.name),
-        subtitle: Text(data.message),
-      );
+  Widget build(BuildContext context) {
+    final firstLetter =
+        data.author.name.isEmpty ? '?' : data.author.name.substring(0, 1);
+
+    var color = Color(0xff000000 | firstLetter.hashCode);
+    if (color.computeLuminance() < 0.4) {
+      color = Color(0x00ffffff ^ color.value);
+    }
+    final color2 = color.computeLuminance() < 0.4 ? Colors.white : Colors.black;
+
+    return ListTile(
+      leading: CircleAvatar(
+        foregroundColor: color2,
+        backgroundColor: color,
+        child: Text(
+          firstLetter,
+        ),
+      ),
+      trailing: Text(data.timestamp.toString()),
+      title: Text(data.author.name),
+      subtitle: Text(data.message),
+      isThreeLine: data.message.contains('\n'),
+    );
+  }
 }
